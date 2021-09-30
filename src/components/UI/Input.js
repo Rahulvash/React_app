cc8b1ff94713f924a0dceab08299638adc4c44f3
Rahulvash <rahulvashishtha1001@gmail.com>
2021-09-30 23:38:31 +0530

    commit

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,10 +1,12 @@
-import { classes } from './Input.module.css';
+import classes from './Input.module.css';
 import React from 'react';
 
 const Input = (props) => {
+     console.log(classes.input);
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id}>{props.label}</label>
+            <input {...props.input}></input>
         </div>
     )
 }
